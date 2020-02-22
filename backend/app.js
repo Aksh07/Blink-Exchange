c7d1cc1efc3b03015bad9106d8755e3f062296f6
Stumblef00l <kunalbonucci@gmail.com>
2020-02-22 12:00:21 +0530

    Fixed postRoutes and added deleteRoutes

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const app = express();
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://kunalverma:kunalverma2017@cluster0-9flyf.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-
 
 // Importing the various routes
 
 const pageRoutes = require("./pageRoutes");
 const getRoutes = require("./getRoutes.js");
+const postRoutes = require("./postRoutes.js");
+const deleteRoutes = require("./deleteRoutes.js");
 
 app.use("/", pageRoutes);
 app.use("/apis/get", getRoutes);
+app.use("/apis/post", postRoutes);
+app.use("/apis/delete", deleteRoutes);
 
 // Starting the server
+
 const port = process.env.PORT || 8080;
 app.listen(port);
 
