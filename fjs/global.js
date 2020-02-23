dfa62479c941bf9b5512381e74cc07c17126f0fb
jatinarora135 <jatinarora135@gmail.com>
2020-02-23 11:18:04 +0530

    Upload image bug is fixed in form file

--- fjs/global.js
+++ fjs/global.js
@@ -1,70 +1,48 @@
 (function ($) {
     'use strict';
-    /*==================================================================
-        [ Daterangepicker ]*/
-    try {
-        $('.js-datepicker').daterangepicker({
-            "singleDatePicker": true,
-            "showDropdowns": true,
-            "autoUpdateInput": false,
-            locale: {
-                format: 'DD/MM/YYYY'
-            },
-        });
-    
-        var myCalendar = $('.js-datepicker');
-        var isClick = 0;
-    
-        $(window).on('click',function(){
-            isClick = 0;
-        });
-    
-        $(myCalendar).on('apply.daterangepicker',function(ev, picker){
-            isClick = 0;
-            $(this).val(picker.startDate.format('DD/MM/YYYY'));
-    
-        });
-    
-        $('.js-btn-calendar').on('click',function(e){
-            e.stopPropagation();
-    
-            if(isClick === 1) isClick = 0;
-            else if(isClick === 0) isClick = 1;
-    
-            if (isClick === 1) {
-                myCalendar.focus();
-            }
-        });
-    
-        $(myCalendar).on('click',function(e){
-            e.stopPropagation();
-            isClick = 1;
-        });
-    
-        $('.daterangepicker').on('click',function(e){
-            e.stopPropagation();
-        });
-    
-    
-    } catch(er) {console.log(er);}
-    /*[ Select 2 Config ]
+
+    /*[ File Input Config ]
         ===========================================================*/
     
     try {
-        var selectSimple = $('.js-select-simple');
     
-        selectSimple.each(function () {
-            var that = $(this);
-            var selectBox = that.find('select');
-            var selectDropdown = that.find('.select-dropdown');
-            selectBox.select2({
-                dropdownParent: selectDropdown
+        var file_input_container = $('.js-input-file');
+    
+        if (file_input_container[0]) {
+    
+            file_input_container.each(function () {
+    
+                var that = $(this);
+    
+                var fileInput = that.find(".input-file");
+                var info = that.find(".input-file__info");
+    
+                fileInput.on("change", function () {
+    
+                    var fileName;
+                    fileName = $(this).val();
+    
+                    if (fileName.substring(3,11) == 'fakepath') {
+                        fileName = fileName.substring(12);
+                    }
+    
+                    if(fileName == "") {
+                        info.text("No file chosen");
+                    } else {
+                        info.text(fileName);
+                    }
+    
+                })
+    
             });
-        });
     
-    } catch (err) {
-        console.log(err);
+        }
+    
+    
+    
     }
-    
+    catch (e) {
+        console.log(e);
+    }
 
 })(jQuery);
