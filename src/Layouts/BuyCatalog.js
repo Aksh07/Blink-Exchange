f653adb1c860c4c17811c112e155c787edfe24a5
kshtjingle <58187077+kshtjingle@users.noreply.github.com>
2020-04-03 11:52:02 +0530

    Update BuyCatalog.js

--- src/Layouts/BuyCatalog.js
+++ src/Layouts/BuyCatalog.js
@@ -29,10 +29,9 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-
+import SearchBar from 'material-ui-search-bar'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
-//var React = require('react');
 
 const classes = {
 	root: {
@@ -77,25 +76,259 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: red[500],
   },
 }));
+classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
 class BuyCatalog extends React.Component{
 	
-	constructor(props){
-		super(props);
-	}
-	render(){
-		return(
-			<Fragment>
+	
+ 
+  constructor(props){
+	  super(props);
+  
+	var items = [{
+		name: 'Bat',
+		type: 'Product',
+		imgURL: 'https://images.unsplash.com/photo-1583265072717-b68d14e15187?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+		lastDate: '03/02/2020',
+		desc: 'tba'
+	}, {
+		name: 'Elephant',
+		type: 'Service',
+		imgURL: 'https://images.unsplash.com/photo-1583265512283-2d72b64974a7?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+		lastDate: '03/02/2020',
+		desc: 'tba'
+	}, {
+		name: 'Bat',
+		type: 'Product',
+		imgURL: 'https://images.unsplash.com/photo-1583265072717-b68d14e15187?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+		lastDate: '03/02/2020',
+		desc: 'tba'
+	}, {
+		name: 'Bat',
+		type: 'Product',
+		imgURL: 'https://images.unsplash.com/photo-1583265072717-b68d14e15187?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+		lastDate: '03/02/2020',
+		desc: 'tba'
+	}, {
+		name: 'Bat',
+		type: 'Product',
+		imgURL: 'https://images.unsplash.com/photo-1583265072717-b68d14e15187?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+		lastDate: '03/02/2020',
+		desc: 'tba'
+	}, {
+		name: 'Bat',
+		type: 'Product',
+		imgURL: 'https://images.unsplash.com/photo-1583265072717-b68d14e15187?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+		lastDate: '03/02/2020',
+		desc: 'tba'
+	}, {
+		name: 'Bat',
+		type: 'Product',
+		imgURL: 'https://images.unsplash.com/photo-1583265072717-b68d14e15187?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+		lastDate: '03/02/2020',
+		desc: 'tba'
+	}, {
+		name: 'Bat',
+		type: 'Product',
+		imgURL: 'https://images.unsplash.com/photo-1583265072717-b68d14e15187?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+		lastDate: '03/02/2020',
+		desc: 'tba'
+	}, {
+		name: 'Bat',
+		type: 'Product',
+		imgURL: 'https://images.unsplash.com/photo-1583265072717-b68d14e15187?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+		lastDate: '03/02/2020',
+		desc: 'tba'
+	}]
+	}	
+render(){
+return (
+    
+      <Fragment>
       <Helmet>
         <style>{"body { background-color: #003366;}"}</style>
         <title>Buy</title>
       </Helmet>
       <Header />
 	  <br/><br/>
+	<Grid
+        container
+        spacing={10}
+        direction="column"
+        justify="center"
+        alignItems="center"
+        style={style.GridColor}
+      >
+        <Grid item direction="row" justify="center" alignItems="center">
+          <Typography style={style.HomeHeader} variant="h2">
+            Blink Exchange
+          </Typography>
+        </Grid></Grid>
+	  <br/><br/> <br/>
+	 
+	  <MuiThemeProvider>
+		  <SearchBar
+      onChange={() => console.log('onChange')}
+      onRequestSearch={() => console.log('onRequestSearch')}
+      style={{
+        margin: '0 auto',
+        maxWidth: 800
+      }}
+			  /></MuiThemeProvider>
+	  								
+	  
+	  <br/><br/> <br/>
+				  
+		<Grid
+        container
+        spacing={10}
+        direction="column"
+        justify="center"
+        alignItems="flex-end"
+        style={style.GridColor}
+		md = "12"
+      >
 			
-			</Fragment>
+			
+        <Grid item direction="row" justify="center" alignItems="center" alignContent = "space-around">
+          <Typography style={style.HomeHeader} variant="h6">
+            Sort by :&nbsp;&nbsp;
+			<select>
+				<option value = "name">Name</option>
+				<option value = "dod">Date of Delivery</option>
+				<option value = "price">Price</option>
+				</select>
+		<svg height = "50" width = "50">
+			<rect x = "10" y = "29" height = "5" width = "30" fill = "#F50057"/>
+			<rect x = "10" y = "37" height = "5" width = "20" fill = "#F50057"/>
+			<rect x = "10" y = "45" height = "5" width = "10" fill = "#F50057"/>
+		</svg>
+          </Typography>
+        </Grid></Grid>
+	  <br/><br/> 		  
+	  
+	   
+	 <div style = {{padding: 50}}><Grid container direction = "row" justify = "space-evenly" md = "12" alignItems = "center" spacing = "8">
+	  	
+	 
+	  {this.items.map(function(item){
+	if(item.type == 'Service'){
+	  	return(
+			<Grid item md = "3" direction = "column">
+				
+			<Card className={classes.root} style = {style.CardColor2}>
+      <CardHeader
+        /*avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            R
+          </Avatar>
+        }*/
+        action={
+          <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        }
+        title={item.name}
+        subheader={item.type}
+      />
+      <CardMedia
+        className={classes.media}
+        image={item.imgURL}
+        title="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Last Date of Delivery: {item.lastDate}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        
+         <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+	<Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>{item.desc}</Typography>
+          
+        </CardContent>
+      </Collapse>
+      
+    </Card></Grid>
+			
 		)
-	}
-}
+  }
+		else{
+			
+			return(
+				<Grid item md = "3" direction = "column">
+				
+			<Card className={classes.root} style = {style.CardColor}>
+      <CardHeader
+        /*avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            R
+          </Avatar>
+        }*/
+        action={
+          <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        }
+        title={item.name}
+        subheader={item.type}
+      />
+      <CardMedia
+        className={classes.media}
+        image={item.imgURL}
+        title="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Last Date of Delivery: {item.lastDate}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        
+         <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+	<Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>{item.desc}</Typography>
+          
+        </CardContent>
+      </Collapse>
+      
+    </Card></Grid>
+			)
+			
+		}
 
-export default BuyCatalog;
+		
+  	})}
+	  </Grid> </div>
+	  </Fragment>
+
+  );}}
+  
+  export default BuyCatalog;
